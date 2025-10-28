d4a8d2cb1cc5ba4e874035542b90b3b7c0e809ac
srini44vasan-hub <srini44vasan@gmail.com>
2025-10-28 13:07:40 +0530

    commit only on local

--- Additioncode.py
+++ Additioncode.py
@@ -2,8 +2,10 @@ f= 100
 g = 200
 sum1 = f + g
 h = 6
-sum = sum1 + h
+g = 20
+sum = sum1 + h + g
 add = sum (sum +sum)
 print ("sum of the numbers =" sum)
 print ("hi how are  you")
 print  ("I am fine")
+print  ("also  today is 28th Oct)
