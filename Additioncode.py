ae84dbd2522b5ac1a0f1ea0a1c959f6ce480b48a
srini44vasan-hub <srini44vasan@gmail.com>
2025-10-28 14:19:38 +0530

    commit dev branch

--- Additioncode.py
+++ Additioncode.py
@@ -4,9 +4,10 @@ sum1 = f + g
 h = 6
 g = 20
 sum = sum1 + h + g
-add = sum (sum +sum)
+add = sum (sum +sum1)
 print ("sum of the numbers =" sum)
 print ("hi how are  you")
 print  ("I am fine")
 print  ("also  today is 28th Oct)
 print  ("all good for today and I hope you too fine")
+print  ( "dev  branch")
