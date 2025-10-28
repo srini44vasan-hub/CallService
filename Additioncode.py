f33f1743d224f8353fcaa1e6587ab326fad80645
srini44vasan-hub <srini44vasan@gmail.com>
2025-10-28 14:14:11 +0530

    dev commit 1

--- Additioncode.py
+++ Additioncode.py
@@ -4,7 +4,6 @@ sum1 = f + g
 h = 6
 g = 20
 e= 20 //dev branch1//
-ee = 30 //msl branch 2//
 sum = sum1 + h + g
 add = sum (sum +sum)
 print ("sum of the numbers =" sum)
@@ -13,4 +12,3 @@ print  ("I am fine")
 print  ("also  today is 28th Oct)
 print  ("all good for today and I hope you too fine")
 print ("dev branch1")
-print ("msl branch")
