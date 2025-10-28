85ed5e06bec4bebe701c91822ad77409a647140e
srini44vasan-hub <srini44vasan@gmail.com>
2025-10-28 14:12:48 +0530

    msl commit local

--- Additioncode.py
+++ Additioncode.py
@@ -3,6 +3,8 @@ g = 200
 sum1 = f + g
 h = 6
 g = 20
+e= 20 //dev branch1//
+ee = 30 //msl branch 2//
 sum = sum1 + h + g
 add = sum (sum +sum)
 print ("sum of the numbers =" sum)
@@ -10,3 +12,5 @@ print ("hi how are  you")
 print  ("I am fine")
 print  ("also  today is 28th Oct)
 print  ("all good for today and I hope you too fine")
+print ("dev branch1")
+print ("msl branch")
